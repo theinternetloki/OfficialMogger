86e3153f7a8a000a5e528cec7a5166faa7e4c9d6
Michael Raafat Sahion <mochaelsahion@gmail.com>
2013-03-03 18:11:04 -0600

    updates

--- src/com/kimistudios/mogger/Register.java
+++ src/com/kimistudios/mogger/Register.java
@@ -1,16 +1,104 @@
 package com.kimistudios.mogger;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+import library.DatabaseHandler;
+import library.UserFunctions;
+ 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+ 
 public class Register extends Activity {
-
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_register);
-	    // TODO Auto-generated method stub
-	}
-
+    Button btnRegister;
+    Button btnLinkToLogin;
+    EditText inputFullName;
+    EditText inputEmail;
+    EditText inputPassword;
+    TextView registerErrorMsg;
+ 
+    // JSON Response node names
+    private static String KEY_SUCCESS = "success";
+    private static String KEY_ERROR = "error";
+    private static String KEY_ERROR_MSG = "error_msg";
+    private static String KEY_UID = "uid";
+    private static String KEY_NAME = "name";
+    private static String KEY_EMAIL = "email";
+    private static String KEY_CREATED_AT = "created_at";
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+ 
+        // Importing all assets like buttons, text fields
+        inputFullName = (EditText) findViewById(R.id.editText1);
+        inputEmail = (EditText) findViewById(R.id.editText2);
+        inputPassword = (EditText) findViewById(R.id.editText3);
+        final EditText address=(EditText) findViewById(R.id.editText4);
+        final EditText birth=(EditText) findViewById(R.id.editText5);        
+        btnRegister = (Button) findViewById(R.id.button1);
+        btnLinkToLogin = (Button) findViewById(R.id.button3);
+ 
+        // Register Button Click event
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String name = inputFullName.getText().toString();
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                String saddress = address.getText().toString();
+                String birthyr = birth.getText().toString();
+                UserFunctions userFunction = new UserFunctions();
+                JSONObject json = userFunction.registerUser(name, email, password, saddress, birthyr);
+ 
+                // check for login response
+                try {
+                    if (json.getString(KEY_SUCCESS) != null) {
+                        registerErrorMsg.setText("");
+                        String res = json.getString(KEY_SUCCESS);
+                        if(Integer.parseInt(res) == 1){
+                            // user successfully registred
+                            // Store user details in SQLite Database
+                            DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+                            JSONObject json_user = json.getJSONObject("user");
+ 
+                            // Clear all previous data in database
+                            userFunction.logoutUser(getApplicationContext());
+                            db.addUser(json_user.getString(KEY_NAME), json_user.getString(KEY_EMAIL), json.getString(KEY_UID), json_user.getString(KEY_CREATED_AT));
+                            // Launch Dashboard Screen
+                            Intent dashboard = new Intent(getApplicationContext(), Dashboard.class);
+                            // Close all views before launching Dashboard
+                            dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(dashboard);
+                            // Close Registration Screen
+                            finish();
+                        }else{
+                            // Error in registration
+                            registerErrorMsg.setText("Error occured in registration");
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+ 
+        // Link to Login Screen
+        btnLinkToLogin.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        Login.class);
+                startActivity(i);
+                // Close Registration View
+                finish();
+            }
+        });
+    }
 }
