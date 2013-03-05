ffe46f99978c27d4f65cf2d9ec8b3f2d675b3d9e
Michael Raafat Sahion <mochaelsahion@gmail.com>
2013-03-04 19:03:09 -0600

    small changes, wonder if the git is fixed

--- gen/com/kimistudios/mogger/R.java
+++ gen/com/kimistudios/mogger/R.java
@@ -17,17 +17,18 @@ public final class R {
         public static final int wood2=0x7f020003;
     }
     public static final class id {
-        public static final int btnLogout=0x7f07000a;
+        public static final int btnLogout=0x7f07000e;
         public static final int button1=0x7f070004;
         public static final int button2=0x7f070005;
         public static final int button3=0x7f070008;
-        public static final int button4=0x7f07000c;
+        public static final int button4=0x7f07000b;
+        public static final int button5=0x7f07000c;
         public static final int editText1=0x7f070001;
         public static final int editText2=0x7f070003;
         public static final int editText3=0x7f070007;
-        public static final int menu_settings=0x7f07000e;
-        public static final int quickContactBadge1=0x7f07000b;
-        public static final int scrollView1=0x7f07000d;
+        public static final int imageView1=0x7f07000d;
+        public static final int menu_settings=0x7f07000f;
+        public static final int scrollView1=0x7f07000a;
         public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070002;
         public static final int textView3=0x7f070006;
